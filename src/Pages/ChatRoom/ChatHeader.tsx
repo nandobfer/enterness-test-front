bd2d34fff8b8c3709a3236ca84b39e0de3789d26
nandobfer <nandobfer@yahoo.com.br>
2024-12-01 15:18:26 -0300

    feat: chat details on menu

--- src/Pages/ChatRoom/ChatHeader.tsx
+++ src/Pages/ChatRoom/ChatHeader.tsx
@@ -1,12 +1,28 @@
 import React, { useState } from "react"
-import { Box, IconButton, Menu, MenuItem, Typography } from "@mui/material"
+import { Box, IconButton, Menu, MenuItem, SvgIconTypeMap, Typography } from "@mui/material"
 import { Chat } from "../../types/class/Chat"
-import { ArrowCircleLeft, Menu as MenuIcon } from "@mui/icons-material"
+import { ArrowCircleLeft, Menu as MenuIcon, Message, Person } from "@mui/icons-material"
 import { useNavigate } from "react-router-dom"
+import { OverridableComponent } from "@mui/material/OverridableComponent"
 
 interface ChatHeaderProps {
     chat: Chat
     onLeave: () => void
+}
+
+export const ChatDetail: React.FC<{
+    icon: OverridableComponent<SvgIconTypeMap<{}, "svg">> & {
+        muiName: string
+    }
+    value: number
+}> = ({ icon, value }) => {
+    const Icon = icon
+    return (
+        <Box sx={{ alignItems: "center", gap: "0.5vw", fontSize: "0.9rem" }}>
+            <Icon sx={{ width: "1vw" }} />
+            {value}
+        </Box>
+    )
 }
 
 export const ChatHeader: React.FC<ChatHeaderProps> = ({ chat, onLeave }) => {
@@ -25,7 +41,16 @@ export const ChatHeader: React.FC<ChatHeaderProps> = ({ chat, onLeave }) => {
                 <MenuIcon />
             </IconButton>
 
-            <Menu anchorEl={menuAnchor} open={!!menuAnchor} onClose={() => setMenuAnchor(null)}>
+            <Menu
+                anchorEl={menuAnchor}
+                open={!!menuAnchor}
+                onClose={() => setMenuAnchor(null)}
+                slotProps={{ paper: { sx: { bgcolor: "background.default" } } }}
+            >
+                <Box sx={{ flexDirection: "column", gap: "0.5vw", padding: "0.5vw" }}>
+                    <ChatDetail icon={Person} value={chat.users.length + 1} />
+                    <ChatDetail icon={Message} value={chat.messages.length} />
+                </Box>
                 <MenuItem onClick={onLeave}>Sair da sala</MenuItem>
             </Menu>
         </Box>
